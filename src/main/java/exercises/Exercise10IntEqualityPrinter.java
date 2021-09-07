63f9bf2b160321ba4b03fbc878891f2bb7aa6d33
Draxawox <kalix321@gmail.com>
2021-09-07 14:49:39 +0200

    exercise 11 added

---
+++ src/main/java/exercises/Exercise10IntEqualityPrinter.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class Exercise10IntEqualityPrinter {
+    public static void printEqual(int a, int b, int c) {
+        System.out.println(
+                (a < 0 || b < 0 || c < 0) ? "Invalid Value" :
+                        (a == b && b == c) ? "All numbers are equal" :
+                                (a != b && b != c && a != c) ? "All numbers are different" :
+                                        "Neither all are equal or different"
+        );
+    }
+}
+
+
---
+++ src/main/java/exercises/Exercise9MinutesToYearsDaysCalculator.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class Exercise9MinutesToYearsDaysCalculator {
+    public static void printYearsAndDays(long minutes) {
+        System.out.println(minutes < 0 ? "Invalid Value" :
+                minutes + " min = " +
+                ((minutes / 60) / 24) / 365 + " y and " +
+                ((minutes /60) / 24) % 365 + " d");
+    }
+}
+
+
