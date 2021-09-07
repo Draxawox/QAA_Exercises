1c8dadef8e9ab91f8e1f8d6bcaf26641350c8636
Draxawox <kalix321@gmail.com>
2021-09-07 15:20:38 +0200

    exercise 12 added

--- src/main/java/exercises/Exercise11PlayingCat.java
+++ src/main/java/exercises/Exercise11PlayingCat.java
@@ -1,4 +1,7 @@
 package exercises;
 
 public class Exercise11PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        return !summer && (temperature >= 25 && temperature <= 35) || (summer && (temperature >= 25 && temperature <= 45));
+    }
 }
